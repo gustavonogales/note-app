833eaf9a4854035b7521ae18f7b8555dec756ca7
Gustavo Oquendo Nogales <gustavo.nogales@hotmail.com>
2023-07-17 10:00:52 -0300

    change static route to /files

--- api/nestjs/src/app.module.ts
+++ api/nestjs/src/app.module.ts
@@ -14,6 +14,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/files',
     }),
     MulterModule.register(),
     ConfigModule.forRoot({
