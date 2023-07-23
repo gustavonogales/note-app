a4a6ed1614ffb14c02f8b27bba69be69ccbfc8d6
Gustavo Oquendo Nogales <gustavo.nogales@hotmail.com>
2023-07-23 15:18:08 -0300

    change postgres to sqlite

--- api/nestjs/src/shared/database/connection.config.ts
+++ api/nestjs/src/shared/database/connection.config.ts
@@ -2,11 +2,7 @@ import { join } from 'path';
 
 export default () => ({
   type: process.env.ORM_PG_TYPE,
-  host: process.env.ORM_PG_HOST,
-  port: process.env.ORM_PG_PORT,
-  username: process.env.ORM_PG_USERNAME,
-  password: process.env.ORM_PG_PASSWORD,
-  database: process.env.ORM_PG_DATABASE,
+  database: 'database.sqlite',
   entities: [join(__dirname, '../../**/*.entity{.ts,.js}')],
   synchronize: true,
 });
