01113c3afa483ad77d47e99b45033bbba5d213a1
Gustavo Oquendo Nogales <gustavo.nogales@hotmail.com>
2023-07-24 13:07:11 -0300

    add support to sqlite

--- api/src/shared/database/connection.config.ts
+++ api/src/shared/database/connection.config.ts
@@ -2,7 +2,14 @@ import { join } from 'path';
 
 export default () => ({
   type: process.env.ORM_PG_TYPE,
-  database: join(__dirname, '..', '..', 'database.sqlite'),
+  host: process.env.ORM_PG_HOST,
+  port: process.env.ORM_PG_PORT,
+  username: process.env.ORM_PG_USERNAME,
+  password: process.env.ORM_PG_PASSWORD,
+  database:
+    process.env.ORM_PG_TYPE == 'sqlite'
+      ? join(__dirname, '..', '..', 'database.sqlite')
+      : process.env.ORM_PG_DATABASE,
   entities: [join(__dirname, '../../**/*.entity{.ts,.js}')],
   synchronize: true,
 });
